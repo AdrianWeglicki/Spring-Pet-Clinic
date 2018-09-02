257e24ec2e849989de7c1f215b241209d1390639
Adrian <adrianw@gmail.com>
2018-09-02 22:46:52 +0100

    adding owners list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/adrianv/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/adrianv/petclinic/PetClinicApplication.java
@@ -3,7 +3,7 @@ package com.adrianv.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "org.adrianv" })
+@SpringBootApplication
 public class PetClinicApplication {
 
     public static void main(String[] args) {
