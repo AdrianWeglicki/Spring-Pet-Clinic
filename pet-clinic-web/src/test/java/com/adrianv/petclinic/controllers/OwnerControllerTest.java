aaed7f01d79c9098dd83cbf1bc45eb77dfd708c7
Adrian <adrianw@gmail.com>
2018-10-02 19:46:09 +0100

    display owner record. Closes #50

--- pet-clinic-web/src/test/java/com/adrianv/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/adrianv/petclinic/controllers/OwnerControllerTest.java
@@ -9,14 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,7 +46,7 @@ class OwnerControllerTest {
     void listOwners() throws Exception {
         when(ownerService.findAll()).thenReturn(owners);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/owners"))
+        mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
                 .andExpect(model().attribute("owners", hasSize(2)));
@@ -54,9 +55,21 @@ class OwnerControllerTest {
     void listOwnersByIndex() throws Exception {
         when(ownerService.findAll()).thenReturn(owners);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/owners/index"))
+        mockMvc.perform(get("/owners/index"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
                 .andExpect(model().attribute("owners", hasSize(2)));
     }
+
+    @Test
+    void displayOwner()throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner",hasProperty("id",is(1l))));
+
+    }
+
 }
